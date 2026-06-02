dd209367ab64effb3412b202b1084303c71d2c46
ajiramandani111-ux <ajiramandani111@gmail.com>
2026-06-02 20:00:57 +0700

    Add files via upload

---
+++ Praktikum15/StackDemo.java
@@ -0,0 +1,39 @@
+import java.util.Stack;
+
+public class StackDemo {
+    public static void main(String[] args) {
+
+        Book book1 = new Book("1234", "Dasar Pemrograman");
+        Book book2 = new Book("7145", "Hafalah Shalat Delisa");
+        Book book3 = new Book("3562", "Muhammad Al-Fatih");
+
+        Stack<Book> books = new Stack<>();
+
+        books.push(book1);
+        books.push(book2);
+        books.push(book3);
+
+        Book temp = books.peek();
+
+        if (temp != null) {
+            System.out.println(temp.toString());
+        }
+
+        Book temp2 = books.pop();
+
+        if (temp2 != null) {
+            System.out.println(temp2.toString());
+        }
+
+        // Nomor 6
+        for (Book book : books) {
+            System.out.println(book.toString());
+        }
+
+        // Nomor 7
+        System.out.println(books);
+
+        // Nomor 8
+        System.out.println("Posisi book1 : " + books.search(book1));
+    }
+}
